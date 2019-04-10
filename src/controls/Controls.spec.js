5fa797f7376f3129c2702638aa19c059a016589a
Sorin Chis <kish.sorin@yahoo.com>
2019-04-10 16:04:48 +0300

    added tests for toggleClosed function

---
+++ src/controls/Controls.spec.js
@@ -0,0 +1,31 @@
+// Test away!
+import React from "react";
+import * as rt from "react-testing-library";
+import Controls from "./Controls";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+describe("Testing Controls component", () => {
+  it("render Conponents component succesfully", () => {
+    rt.render(<Controls />);
+  });
+
+  it("it should render the buttons succesfully", () => {
+    const wrap = rt.render(<Controls />);
+    const lockBtn = wrap.getByText(/Lock Gate/i);
+    const closeBtn = wrap.getByText(/Close Gate/i);
+    expect(lockBtn).toBeDefined();
+    expect(closeBtn).toBeDefined();
+  });
+
+  it("execute function toggleClosed when toggle is pressed", () => {
+    const toggleClosed = jest.fn();
+    const wrap = rt.render(<Controls toggleClosed={toggleClosed} disable={false} />);
+    const button = wrap.getByText(/Close Gate/i);
+    rt.fireEvent.click(button);
+    expect(toggleClosed).toHaveBeenCalledTimes(1);
+    rt.fireEvent.click(button);
+    expect(toggleClosed).toHaveBeenCalledTimes(2);
+
+  });
+});
