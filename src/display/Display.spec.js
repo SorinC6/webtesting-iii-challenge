00968ebe0dd660ca48537ead3ad159a216831591
Sorin Chis <kish.sorin@yahoo.com>
2019-04-10 14:48:57 +0300

    added tests for Display Component when closed prop is false or true

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from "react";
+import Display from "./Display";
+import "jest-dom/extend-expect";
+import * as rt from "react-testing-library";
+
+describe("The Display Component", () => {
+  it("render without crasing", () => {
+    rt.render(<Display />);
+  });
+
+  it("when prop closed is false  - display: Open", () => {
+    const wrap = rt.render(<Display closed={false} />);
+    const closed = wrap.getByTestId("closedDisplay");
+    expect(closed).toHaveTextContent(/open/i);
+  });
+
+  it("when prop closed is true  - display: Closed", () => {
+    const wrap = rt.render(<Display closed={false} />);
+    const open = wrap.getByText(/open/i);
+    expect(open).toBeDefined();
+  });
+});
